a127b09d70c1eb37b278ae9397bce536e6d5bd49
Onkor114 <abdinourahmed114@gmail.com>
2024-10-21 16:10:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic form submission handling
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+});
